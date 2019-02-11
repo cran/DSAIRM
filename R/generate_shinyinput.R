3be7fdaf641c6af38cacf804903937a4fe401ab9
Andreas Handel <ahandel@uga.edu>
2019-02-11 20:33:37 +0000

    version 0.5.5

--- R/generate_shinyinput.R
+++ R/generate_shinyinput.R
@@ -2,20 +2,18 @@
 #'
 #' @description This function generates numeric shiny UI inputs for a supplied model.
 #' This is a helper function called by the shiny app.
-#' @param mbmodel a name of a file/function or a modelbuilder model structure
-#' @param otherinputs a list of other inputs to include
+#' @param mbmodel a name of a function for which to build inputs
+#' @param otherinputs a list of other shiny inputs to include
 #' @param packagename name of package using this function
-#' @param output shiny output structure
-#' @return No direct return. output structure is modified to contain text for display in a Shiny UI
+#' @return A renderUI object that can be added to the shiny output object for display in a Shiny UI
 #' @details This function is called by the Shiny app to produce the Shiny input UI elements.
-#' If mbmodel is an object, it is assumed to be a mbmodel type and will be parsed to create the UI elements.
-#' If mbmodel is a character, it is assumed to be the name of a function which will be parsed to create UI elements.
+#' mbmodel is assumed to be the name of a function which will be parsed to create UI elements.
 #' Non-numeric arguments of functions are removed.
 #' @author Andreas Handel
 #' @export
 
 #not used in DSAIRM, might need to turn on again for modelbuilder
-generate_shinyinput <- function(mbmodel, otherinputs, packagename, output)
+generate_shinyinput <- function(mbmodel, otherinputs = NULL, packagename)
 {
 
     #function to wrap input elements in specified class
@@ -33,9 +31,13 @@ generate_shinyinput <- function(mbmodel, otherinputs, packagename, output)
     #get every line in documentation part of file that starts with @param
     x = readLines(fcfile)
     x2 = grep('@param', x, value = TRUE)
-    pattern = ":.+:" #regex for capturing text between colons
-    x3 = stringr::str_extract_all(x2, pattern, simplify = TRUE)
-    x3=substr(x3,3,nchar(x3)-2); #remove : and blanks in front and back
+    pattern = ".*[:](.+)[:].*" #regex for capturing text between colons
+    x3 = gsub(pattern, "\\1",x2)
+    x3 = substr(x3,2,nchar(x3)-1); #remove blanks in front and back
+    #using stringr
+    #pattern = ":.+:" #regex for capturing text between colons
+    #x3 = stringr::str_extract_all(x2, pattern, simplify = TRUE)
+    #x3=substr(x3,3,nchar(x3)-2); #remove : and blanks in front and back
     ip = formals(mbmodel) #get model inputs
     #remove function arguments that are not numeric
     ip = ip[unlist(lapply(ip,is.numeric))]
@@ -48,18 +50,20 @@ generate_shinyinput <- function(mbmodel, otherinputs, packagename, output)
         ) #close myclassfct
     }) #close lapply
 
+    otherargs = NULL
     if (!is.null(otherinputs))
     {
         otherargs = lapply(otherinputs,myclassfct)
     }
 
     #return structure
-    output$modelinputs <- renderUI({
-        tagList(
+    modelinputs <- tagList(
             p(actionButton("submitBtn", "Run Simulation", class = "submitbutton"), align = 'center'),
             modelargs,
             otherargs
         ) #end tagList
-    }) #end renderuI
+
+    return(modelinputs)
+
 } #end overall function
 
--- inst/doc/DSAIRM.R
+++ inst/doc/DSAIRM.R
@@ -11,30 +11,31 @@ library('DSAIRM')
 #  help('simulate_basicbacteria_ode')
 
 ## ---- eval=TRUE, echo=TRUE-----------------------------------------------
-result <- simulate_basicbacteria_ode(B = 500, I = 5, g = 1, r = 0.002, dI = 1 )
+result <- simulate_basicbacteria_ode(g = 0.5, r = 0.002)
 
 ## ---- eval=TRUE, echo=TRUE-----------------------------------------------
-plot(result$ts[,"time"],result$ts[,"B"],xlab='time',ylab='Bacteria Numbers',type='l')
+plot(result$ts[,"time"],result$ts[,"B"],xlab='Time',ylab='Bacteria Numbers',type='l')
 
 ## ---- eval=TRUE, echo=TRUE-----------------------------------------------
-rvec = 10^seq(-5,-2,length=20) #values of log immune activation rate, r, for which to run the simulation 
-peak = rep(0,length(rvec)) #this will record the peak values for each r
-for (n in 1:length(rvec))
+#values for immune activation rate, r, for which to run the simulation
+rvec = 10^seq(-5,-2,length=20)  
+#this variable will hold the peak values for each r
+Bpeak = rep(0,length(rvec)) 
+for (n in 1:length(rvec))   
 {
-  #call the simulator function with different values of r, other parameters stay at their defaults
+  #run the simulation model for different values of r 
+  #for simplicity, all other inputs are kept at their defaults
   result <- simulate_basicbacteria_ode(r = rvec[n])
-  peak[n] <- max(result$ts[,"B"]) #record max number of bacteria for each value of r
+  #record max number of bacteria for each value of r
+  Bpeak[n] <- max(result$ts[,"B"]) 
 }
 #plot final result
-plot(rvec,peak,type='p',xlab='Immune activation rate',ylab='Max number of bacteria',log='xy')
-
-## ---- eval=FALSE, echo=TRUE----------------------------------------------
-#  system.file("simulatorfunctions", package = "DSAIRM")
+plot(rvec,Bpeak,type='p',xlab='Immune activation rate',ylab='Peak bacteria',log='xy')
 
 ## ----eval=FALSE, echo=TRUE-----------------------------------------------
 #  simulate_basicbacteria_ode <- function(B = 10, I = 1, g = 1, Bmax = 1e+05, dB = 0.1, k = 1e-06, r = 0.001, dI = 1, tstart = 0, tfinal = 30, dt = 0.05)
 
-## ----eval=FALSE, echo=TRUE, color='red'----------------------------------
+## ----eval=FALSE, echo=TRUE-----------------------------------------------
 #  mysimulator <- function(B = 10, I = 1, g = 1, Bmax = 1e+05, dB = 0.1, k = 1e-06, r=1e3, dI=1, tstart = 0, tfinal = 30, dt = 0.05, s=1E3)
 
 ## ----eval=FALSE, echo=TRUE-----------------------------------------------
@@ -47,17 +48,24 @@ plot(rvec,peak,type='p',xlab='Immune activation rate',ylab='Max number of bacter
 #  dBdt = g*B*(1-B/Bmax) - dB*B - k*B*I
 #  dIdt = r*B*I - dI*I
 
-## ----eval=FALSE, echo=TRUE, color='red'----------------------------------
+## ----eval=FALSE, echo=TRUE-----------------------------------------------
 #  dBdt = g*B*(1-B/Bmax) - dB*B - k*B*I
 #  dIdt = r*B*I/(s+B) - dI*I
 
 ## ----eval=TRUE, echo=TRUE------------------------------------------------
-source('mysimulator.R') #to initialize the new function - it needs to be in the working directory
-svec = 10^seq(-3,3,length=20) #values of saturation parameter 
-Imax = rep(0,length(svec)) #this will record the maximum immune response level
+#initialize the new function
+#it needs to be in the same directory as this code
+source('mysimulator.R') 
+#values of saturation parameter to explore
+svec = 10^seq(-3,3,length=20)  
+#this will record the maximum immune response level
+Imax = rep(0,length(svec)) 
 for (n in 1:length(svec))
 {
-  result <- mysimulator(s = svec[n]) #all other inputs are kept at their defaults
+  #run the simulation model for different values of s 
+  #for simplicity, all other inputs are kept at their defaults
+  result <- mysimulator(s = svec[n]) 
+  #record max immune response for each value of s
   Imax[n] <- max(result$ts[,"I"])
 }
 plot(svec,Imax,type='p',xlab='Saturation parameter',ylab='Max immune response level',log='xy')
